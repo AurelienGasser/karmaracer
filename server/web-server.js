7684dbf0ccef2cebcecf9b03822885955f1a2013
pouya <pouyajoon@gmail.com>
2011-10-23 01:45:31 +0200

    public

--- server/web-server.js
+++ server/web-server.js
@@ -6,21 +6,26 @@ io.set('log level', 1);
 app.listen(8080);
 
 app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({secret:"grand mere"}));
-    app.use(app.router);
-		app.use(express.errorHandler({ dump: true, stack: true }));
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.session({secret:"grand mere"}));
+  app.use(app.router);
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 });
 
 
 app.dynamicHelpers({
-	'session' : function(req, res) {
-		return req.session;
-	},
-	'flash' : function(req, res) {
-		return req.flash();
-	}
+  'session' : function(req, res) {
+    return req.session;
+  },
+  'flash' : function(req, res) {
+    return req.flash();
+  }
 });
+
+
 
