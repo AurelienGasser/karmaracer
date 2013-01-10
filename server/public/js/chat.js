401285a99b9c771fbca0c89d21631d61419fce62
Pouya <pouya@pouyaair.home>
2013-01-10 21:43:05 +0100

    chat is back

--- server/public/js/chat.js
+++ server/public/js/chat.js
@@ -1,7 +1,7 @@
 function sendMsg() {
   if ($('#chat_input').val().trim() != '') {
     var msg = $('#player_name').val() + ': ' + $('#chat_input').val();
-    game.SocketManager.getConnection().emit('chat', msg);
+    gameInstance.socketManager.getConnection().emit('chat', msg);
   }
   $('#chat_input').val('');
 }
