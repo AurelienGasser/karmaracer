8df6504f9bbc713f293805f42fe35c813ce1d0b0
Aurélien Gasser <aurelien.gasser@transcovo.com>
2013-01-21 00:10:59 +0100

    fix case sensitivity

---
+++ server/classes/PhysicsEngine/Car.js
@@ -0,0 +1,53 @@
+var backbone = require('backbone');
+var _ = require('underscore');
+var sys = require("sys");
+var b2d = require("box2d");
+
+
+var Car = require("./PhysicsItem").extend({
+  startPosition: {
+    x: 10.0,
+    y: 11.5,
+  },
+  urlRoot: '/cars',
+  initialize: function(playerCar) {
+    var a = {
+      physicsEngine: playerCar.gameServer.physicsEngine,
+      position: this.startPosition,
+      size: {
+        w: 1,
+        h: 0.5
+      },
+      density: 1,
+      friction: 0.2
+    };
+    this.playerCar = playerCar;
+    this.name = 'car';
+    this.constructor.__super__.initialize.apply(this, [a]);
+    this.tireResistance = 1.8;
+    this.score = 0;
+  },
+  accelerationMax: 50,
+  accelerate: function(ac) {
+    var acc_helper = 4;
+    var v = {
+      x: acc_helper * ac * Math.cos(this.getAngle()),
+      y: acc_helper * ac * Math.sin(this.getAngle())
+    };
+    //console.log(v);
+    this.applyForceToBody(v);
+  },
+  updatePos: function() {
+    this.reduceVelocityOfBody(this.tireResistance);
+  },
+  receiveHit: function() {
+    this.playerCar.receiveHit();
+  },
+  getShared: function() {
+   var res =  this.constructor.__super__.getShared.bind(this)();
+   res.playerName = this.playerCar.playerName;
+   return res;
+  }
+});
+
+module.exports = Car;
---
+++ server/classes/PhysicsEngine/PhysicsItem.js
@@ -0,0 +1,97 @@
+var backbone = require('backbone');
+var _ = require('underscore');
+var b2d = require("box2d");
+
+var PhysicsItem = backbone.Model.extend({
+  urlRoot: '/physicsItem',
+  initialize: function(_arguments) {
+    this.id = Math.floor(Math.random() * 1e100);
+    this.size = {
+      w: _arguments['size'].w,
+      h: _arguments['size'].h
+    };
+    if(!_.isUndefined(_arguments['name'])) {
+      this.name = _arguments['name'];
+      //console.log('name', this.name);
+    }
+    this.body = _arguments['physicsEngine'].createSquareBody(this, _arguments['position'], this.size, _arguments['density'], _arguments['friction']);
+    this.engine = _arguments['physicsEngine'];
+  },
+  getPosition: function() {
+    if(this.body != null) {
+      return this.body.GetPosition();
+    }
+    return {
+      x: 200,
+      y: 200
+    };
+  },
+  getVector: function(power) {
+    var angle = this.getAngle();
+    //console.log(power, this.getAngle());
+    var v = {
+      x: power.x * Math.cos(angle),
+      y: power.y * Math.sin(angle)
+    };
+    return v;
+  },
+  getSize: function() {
+    return this.size;
+  },
+  addAngle: function(a) {
+    if(this.body !== null) {
+      this.body.m_angularVelocity += a;
+    }
+  },
+  getAngle: function() {
+    if(this.body !== null) {
+      return this.body.GetAngle();
+    }
+    return 0;
+  },
+  turn: function(side) {
+    var angleToAdd = side * Math.PI / 8;
+    //    console.log('b', this.getAngle(), side, this.body !== null, angleToAdd);
+    this.addAngle(angleToAdd);
+    //    console.log('a', this.getAngle());
+  },
+  getShared: function() {
+    var pos = this.getPosition();
+
+    //console.log(this.name);
+    var share = {
+      x: pos.x * this.engine.gScale,
+      y: pos.y * this.engine.gScale,
+      r: this.getAngle(),
+      w: this.size.w * this.engine.gScale,
+      h: this.size.h * this.engine.gScale
+    };
+    if(!_.isUndefined(this.name)) {
+      share.name = this.name;
+      //console.log('name s', this.name);
+    }
+    return share;
+  },
+  applyForceToBody: function(v) {
+    //console.log(this.body);
+    if(!_.isUndefined(this.body) && this.body !== null) {
+      var pos = this.body.GetPosition();
+      this.body.ApplyImpulse(v, {
+        x: pos.x,
+        y: pos.y
+      });
+    }
+  },
+  reduceVelocityOfBody: function(reduceBy) {
+    if(this.body != null) {
+      this.body.m_linearVelocity.x /= 1 * reduceBy;
+      this.body.m_linearVelocity.y /= 1 * reduceBy;
+      this.body.m_angularVelocity /= 1 * reduceBy;
+      if(Math.abs(this.body.m_linearVelocity.x) < 0.005) this.body.m_linearVelocity.x = 0;
+      if(Math.abs(this.body.m_linearVelocity.y) < 0.005) this.body.m_linearVelocity.y = 0;
+      if(Math.abs(this.body.m_angularVelocity) < 0.005) this.body.m_linearVelocity.y = 0;
+    }
+  }
+});
+
+module.exports = PhysicsItem;
