0cb8512bc07c93a20d3d78eee2a43acd8ed838dc
Aurélien Gasser <aurelien.gasser@transcovo.com>
2013-02-09 17:32:47 +0100

    fixed impossible to kill another car

--- server/classes/PhysicsEngine/ContactListener.js
+++ server/classes/PhysicsEngine/ContactListener.js
@@ -1,60 +1,67 @@
 var box2d = require('box2dweb-commonjs');
 
-console.log('loading contact listener...')
-
-var b2ContactListener = box2d.Box2D.Dynamics.b2ContactListener;
-var listener = new b2ContactListener;
-
-function swap(o1, o2, callback) {
-  callback(o1, o2);
-  callback(o2, o1);
+var ContactListener = function(gameServer) {
+  this.gameServer = gameServer;
+  this.init();
 }
 
-function sensorVSwall(f1, f2) {
-  return (f1.IsSensor() && (['wall', 'stone'].indexOf(f2.GetBody().GetUserData().name) != -1))
-}
+ContactListener.prototype.init = function() {
+  var b2ContactListener = box2d.Box2D.Dynamics.b2ContactListener;
+  this.listener = new b2ContactListener;
+  var that = this;
 
-listener.BeginContact = function(contact) {
-  var f1 = contact.GetFixtureA();
-  var f2 = contact.GetFixtureB();
-  var o1 = f1.GetBody().GetUserData();
-  var o2 = f2.GetBody().GetUserData();
-  swap(o1, o2, function(o1, o2){
-    if(sensorVSwall(f1, f2)) {
-      if (o1.playerCar && o1.playerCar.isBot) {
-        o1.playerCar.player.sensorBegin();
+  function swap(o1, o2, callback) {
+    callback(o1, o2);
+    callback(o2, o1);
+  }
+
+  function sensorVSwall(f1, f2) {
+    return (f1.IsSensor() && (['wall', 'stone'].indexOf(f2.GetBody().GetUserData().name) != -1))
+  }
+
+  this.listener.BeginContact = function(contact) {
+    var f1 = contact.GetFixtureA();
+    var f2 = contact.GetFixtureB();
+    var o1 = f1.GetBody().GetUserData();
+    var o2 = f2.GetBody().GetUserData();
+    swap(o1, o2, function(o1, o2){
+      if(sensorVSwall(f1, f2)) {
+        if (o1.playerCar && o1.playerCar.isBot) {
+          o1.playerCar.player.sensorBegin();
+        }
+      } else if (o1.name === 'bullet' && o2.name !== 'bullet'
+       || o1.name === 'rocket' && o2.name !== 'rocket') {
+         if (o1.playerCar != o2.playerCar) {
+           o1.explode(contact.GetFixtureA().GetBody().GetPosition());
+         }
       }
-    } else if (o1.name === 'bullet' && o2.name !== 'bullet'
-     || o1.name === 'rocket' && o2.name !== 'rocket') {
-       if (o1.playerCar != o2.playerCar) {
-         o1.explode(contact.GetFixtureA().GetBody().GetPosition());
-       }
-    } else if(o2.name === 'car' && (o1.name === 'bullet' || o1.name == 'rocket')) {
-      if (o1.playerCar != o2.playerCar) {
-        gameServer.carManager.projectileHitCar(o1.playerCar, o2.playerCar, o1)
+      if (o2.name === 'car' && (o1.name === 'bullet' || o1.name == 'rocket')) {
+        if (o1.playerCar != o2.playerCar) {
+          that.gameServer.carManager.projectileHitCar(o1.playerCar, o2.playerCar, o1)
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-listener.EndContact = function(contact) {
-  var f1 = contact.GetFixtureA();
-  var f2 = contact.GetFixtureB();
-  var o1 = f1.GetBody().GetUserData();
-  var o2 = f2.GetBody().GetUserData();
-  swap(o1, o2, function(o1, o2){
-    if(sensorVSwall(f1, f2)) {
-      if (o1.playerCar && o1.playerCar.isBot) {
-        o1.playerCar.player.sensorEnd();
+  this.listener.EndContact = function(contact) {
+    var f1 = contact.GetFixtureA();
+    var f2 = contact.GetFixtureB();
+    var o1 = f1.GetBody().GetUserData();
+    var o2 = f2.GetBody().GetUserData();
+    swap(o1, o2, function(o1, o2){
+      if(sensorVSwall(f1, f2)) {
+        if (o1.playerCar && o1.playerCar.isBot) {
+          o1.playerCar.player.sensorEnd();
+        }
       }
-    }
-  });
+    });
+  }
+
+  this.listener.PostSolve = function(contact, impulse) {
+  }
+
+  this.listener.PreSolve = function(contact, oldManifold) {
+  }
 }
 
-listener.PostSolve = function(contact, impulse) {
-}
-
-listener.PreSolve = function(contact, oldManifold) {
-}
-
-module.exports = listener;
+module.exports = ContactListener;
