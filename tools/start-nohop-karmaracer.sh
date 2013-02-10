c0299a9a3c16621d72fde26c6e88d77c6092cb09
Pouya <pouyajoon@gmail.com>
2013-02-10 15:59:27 +0100

    fix nohup script

--- tools/start-nohop-karmaracer.sh
+++ tools/start-nohop-karmaracer.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-git pull origin master
 killall node
-cd ./server
-nohup node server.js > server.nohup.out 2> server.log.err < /dev/null &
+cd ./server && nohup node server.js > server.nohup.out 2> server.log.err < /dev/null &
