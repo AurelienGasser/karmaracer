63a0d2ae43d90457b4a2f182cce8f83be78e616e
Pouya <pouyajoon@gmail.com>
2013-02-10 14:47:49 +0100

    fix deploy

--- tools/start-nohop-karmaracer.sh
+++ tools/start-nohop-karmaracer.sh
@@ -2,4 +2,4 @@
 git pull origin master
 killall node
 cd ../server
-nohup node server.js > server.nohup.out 2> server.log.err < /dev/null &
+# nohup node server.js > server.nohup.out 2> server.log.err < /dev/null &
