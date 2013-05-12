87acb3cac58239554663b5b5c394c52554ed73d4
Pouya <pouyajoon@gmail.com>
2013-05-12 16:31:00 +0200

    update cert

--- keys/create_cert.sh
+++ keys/create_cert.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-openssl req -new -x509 -nodes -days 365 -newkey rsa:1024 -keyout $1.key -out $1.crt -config openssl.cnf
+#openssl req -new -x509 -nodes -days 365 -newkey rsa:1024 -keyout $1.key -out $1.crt -config openssl.cnf
 #openssl req -x509 -nodes -days 365 -subj '/C=FR/ST=Paris/L=XII/CN=karma.origamix.fr/O=Origamix/OU=Studios' -newkey rsa:1024 -keyout karma.key -out karma.crt
+openssl req -out startssl.csr -new -newkey rsa:2048 -nodes -keyout startssl.key
