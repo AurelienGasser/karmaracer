898757d02e63e3636121633950c5c479e91505a5
Aurélien Gasser <aurelien.gasser@transcovo.com>
2013-07-05 14:18:22 -0400

    client clock now provides server time for any client time

--- public/src/game/GameInstance/Clock.js
+++ public/src/game/GameInstance/Clock.js
@@ -18,11 +18,16 @@
     if (typeof this.clientSentTs === 'undefined'
     || clientSentTs > this.clientSentTs) {
       this.ping = ping;
+      // server time was this.serverTs when client time was this.clientReceivedTs
       this.serverTs = Math.floor(serverReceivedTs + this.ping / 2);
       this.clientReceivedTs = clientReceivedTs;
       this.clientSentTs = clientSentTs;
       $('#ping').html('ping: ' + this.ping + 'ms');
     }
+  }
+
+  Clock.prototype.getServerTsForClientTs = function(clientTs) {
+    return this.serverTs + (clientTs - this.clientReceivedTs);
   }
 
   Karma.Clock = Clock;
