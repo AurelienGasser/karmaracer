e04e93525e1eb7e013cf874f4cb0934a38638d53
Pouya <pouyajoon@gmail.com>
2013-07-07 21:15:15 +0200

    funny home

--- public/src/home/CarParallax.js
+++ public/src/home/CarParallax.js
@@ -1,6 +1,9 @@
 (function() {
   "use strict";
 
+
+  var winner;
+
   function setPCars() {
     var $objWindow = $(window);
     $('.pcar').each(function() {
@@ -13,8 +16,24 @@
         // var n = (Math.random() * 1e8) % ;
         var n = parseInt($bgObj.attr('data-speed'), 10);
         var max = $(document).width() - $objWindow.width() + n;
-
         var xPos = (sr / max);
+
+        var i = parseInt($bgObj.attr('data-position'), 10);
+        if (i === winner) {
+
+          $('#maps li').each(function(i, li) {
+            var $li = $(li);
+            var l = $li.position().left;
+            var r = l + $li.width();
+            var x = xPos * $(document).width();
+            if (x < r && x > l) {
+              $li.addClass('centerFocus');
+            } else {
+              $li.removeClass('centerFocus');
+            }
+          });
+        }
+
         var coords = xPos * 100 + '% 10%';
         // Change the position of background
         $bgObj.css({
@@ -36,16 +55,16 @@
     return speed;
   }
 
-  function getWinner(len){
-    return + parseInt((Math.random() * 1e5) % len, 10);
+  function getWinner(len) {
+    return +parseInt((Math.random() * 1e5) % len, 10);
   }
 
   function resetSpeed() {
-    var winner = getWinner($('.pcar').length);
+    winner = getWinner($('.pcar').length);
     $('.pcar').each(function(i) {
       var $c = $(this);
       var speed = getSpeed();
-      if (i === winner){
+      if (i === winner) {
         speed = 0;
       }
       $c.attr('data-speed', speed);
@@ -61,7 +80,7 @@
           var o = [];
           var bottom = i * 48;
           var speed = 0;
-          o.push('<div id="p', c.name, '" class="pcar"  style="bottom:', bottom, 'px;background-image: url(\'', c.path, '\');" data-speed="', speed, '"></div>');
+          o.push('<div id="p', c.name, '" class="pcar" data-position="', i, '"  style="bottom:', bottom, 'px;background-image: url(\'', c.path, '\');" data-speed="', speed, '"></div>');
           $b.append($(o.join('')));
         }
         setPCars();
