7d7f1f519f36af770aad852e27fcea1284f53ec8
Aurélien Gasser <aurelien.gasser@transcovo.com>
2013-07-10 15:26:17 -0400

    all local engine bodies are displayed

--- public/src/common/drawEngine/2DCanvas.js
+++ public/src/common/drawEngine/2DCanvas.js
@@ -289,25 +289,22 @@
     }
   };
 
-  Engine2DCanvas.prototype.drawStaticItems_localPhysicsEngine = function(ctx) {
+  Engine2DCanvas.prototype.drawLocalPhysicsEngineBodies = function(ctx) {
     var that = this;
     if (that.gameInstance.engine.staticBodies !== null) {
       _.each(that.gameInstance.engine.staticBodies, function(c) {
-        var staticItemType = that.worldInfo.staticItemTypes[c.name];
-        if (!KLib.isUndefined(staticItemType) && !KLib.isUndefined(staticItemType.pattern)) {
-          c = {
-            x: c.x * that.gScaleValue,
-            y: c.y * that.gScaleValue,
-            w: c.w * that.gScaleValue,
-            h: c.h * that.gScaleValue
-          };
-          ctx.lineWidth = 4;
-          ctx.strokeStyle = "#00FF00";
-          ctx.strokeRect(c.x - c.w / 2, c.y - c.h / 2, c.w, c.h);
-        }
+        c = {
+          x: c.x * that.gScaleValue,
+          y: c.y * that.gScaleValue,
+          w: c.w * that.gScaleValue,
+          h: c.h * that.gScaleValue
+        };
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "#00FF00";
+        ctx.strokeRect(c.x - c.w / 2, c.y - c.h / 2, c.w, c.h);
       });
     }
-  };
+  }
 
   Engine2DCanvas.prototype.drawBackground = function(ctx) {
     if (KLib.isUndefined(this.backgroundPattern)) {
@@ -323,7 +320,7 @@
     this.drawOutsideWalls(this.ctx);
     this.drawStaticItems(this.ctx);
     if ($('#show_local_physics_engine_bodies').is(':checked')) {
-      this.drawStaticItems_localPhysicsEngine(this.ctx);
+      this.drawLocalPhysicsEngineBodies(this.ctx);
     }
     this.drawCars(this.ctx);
     this.drawExplosions(this.ctx);
