5d48b1c81131154a610261cbe0213e5784cea698
Aurelien Gasser <aurelien.gasser@gmail.com>
2015-12-25 00:12:40 +0100

    update webgl utils + glMatrix

--- public/src/vendor/webgl/webgl-utils.js
+++ public/src/vendor/webgl/webgl-utils.js
@@ -159,4 +159,16 @@ return {
 };
 }();
 
-
+/**
+ * Provides requestAnimationFrame in a cross browser way.
+ */
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+           window.setTimeout(callback, 1000/60);
+         };
+})();
