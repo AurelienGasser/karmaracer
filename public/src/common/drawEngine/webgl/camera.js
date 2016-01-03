cd270d6d4b0f4a1a3973c4e22872b98750875434
Aurelien Gasser <aurelien.gasser@gmail.com>
2016-01-03 00:56:37 +0000

    set camera at interpolated car position

--- public/src/common/drawEngine/webgl/camera.js
+++ public/src/common/drawEngine/webgl/camera.js
@@ -15,17 +15,41 @@
       return;
     } 
     
-    var distFromCamera = 2;
-    var r = this.gameInstance.myCar.r;
-    this.camera.x = this.gameInstance.myCar.x - distFromCamera * Math.cos(r);
-    this.camera.y = this.gameInstance.myCar.y - distFromCamera * Math.sin(r);
-    this.camera.r = r;
+    this.updateCameraPosition();
     
     mat4.rotate(this.mvMatrix, this.mvMatrix, -degToRad(this.camera.pitch), [1, 0, 0]);    
     mat4.rotate(this.mvMatrix, this.mvMatrix, Math.PI / 2, [0, 0, 1]);      
     mat4.rotate(this.mvMatrix, this.mvMatrix, -this.camera.r, [0, 0, 1]);      
-
-    mat4.translate(this.mvMatrix, this.mvMatrix, [-this.camera.x, -this.camera.y, -this.camera.z]);    
+    mat4.translate(this.mvMatrix, this.mvMatrix, [-this.camera.x, -this.camera.y, -this.camera.z]);        
+  };
+  
+  EngineWebGL.prototype.updateCameraPosition = function() {
+    var interpData = this.interpolator.interpData;
+    if (!interpData.ready) {
+      return;
+    }
+    var cars = interpData.snapAfter.cars;
+    if (!cars) {
+      return;
+    }
+    for (var j in cars) {
+      if (j != this.gameInstance.myCar.id) {
+        continue;
+      }
+      var car = cars[j];
+      var carPos = this.interpPosOfCar(j);
+      var player = this.gameInstance.gameInfo[car.id];
+      if (player) {
+        carPos.id = car.id;
+        this.gameInstance.engine.replaceCarBody(carPos);
+        if (!car.dead) {
+          var distFromCamera = 2;
+          this.camera.x = carPos.x - distFromCamera * Math.cos(carPos.r);
+          this.camera.y = carPos.y - distFromCamera * Math.sin(carPos.r);
+          this.camera.r = carPos.r;    
+        }
+      }
+    }    
   };
 
 }(Karma.EngineWebGL));
